f0e08103b9e035107091f3b3f9b8dc048c5369ad
Dmitry Loginov <Dmitry.Loginov@gonitro.com>
2017-04-04 14:07:01 +0100

    upload sources

---
+++ ps_index/ps_index/ps_index.cpp
@@ -0,0 +1,17 @@
+// ps_index.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+void print()
+{
+    std::cout << "test output\n";
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    print();
+	return 0;
+}
+
