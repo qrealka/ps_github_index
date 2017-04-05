f2d1b4e4376fa2f5ca6d0533fb4d4097480fb4d7
Dmitry Loginov <Dmitry.Loginov@gonitro.com>
2017-04-05 14:55:11 +0100

    -update submodule -remove hardcoded paths

---
+++ index.cmd
@@ -0,0 +1,12 @@
+set pdbPath=%1
+set pdbPath=%pdbPath:~0,-2%"
+
+set srcPath=%~dp0
+set srcPath="%srcPath:~0,-1%"
+
+mkdir "%TEMP%\srcsrv"
+xcopy /s /y %2 "%TEMP%\srcsrv"
+
+cd ps
+index.cmd -userID qrealka -repository ps_github_index -sourcesRoot %srcPath% -dbgToolsPath "%TEMP%\srcsrv" -symbolsFolder %pdbPath%
+
